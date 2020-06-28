cd1597828728f7f8662fbe130fc6dad54ae4cb15
DavidGF <DavidGuFu@gmail.com>
2020-06-28 19:04:33 +0200

    sql de proyecto (no puesto como crear la BD)

---
+++ ramon/proyecto/sql proyecto.sql
@@ -0,0 +1,24 @@
+--Create table respuestas
+--(Id_respuesta integer not null primary key,
+-- enunciado_respuesta varchar (999),
+-- respuesta_correcta integer,
+-- Id_pregunta integer);
+
+--create table preguntas
+--(Id_pregunta integer not null primary key,
+-- enunciado_pregunta varchar (999),
+-- Id_test integer,
+-- Id_categoria_p integer);
+
+--Create Table Test
+--(Id_Test integer not null primary key,
+--Descripcion varchar (999),
+--fecha date,
+--Id_categoria_T integer);
+
+--Create Table Categorias
+--(Id_Categoria integer not null primary key,
+--Id_categoria_T integer,
+--Id_categoria_p integer,
+--Descripcion varchar (999),
+--Nombre Varchar (999))
