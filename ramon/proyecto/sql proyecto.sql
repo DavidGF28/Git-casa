d5482d92a045eaf0c6f536a05c8eaf09da33d16b
DavidGF <DavidGuFu@gmail.com>
2020-06-29 13:43:40 +0200

    lios para hacer forenign keys

--- ramon/proyecto/sql proyecto.sql
+++ ramon/proyecto/sql proyecto.sql
@@ -1,3 +1,5 @@
+-- create Database proyecto;
+
 --Create table respuestas
 --(Id_respuesta integer not null primary key,
 -- enunciado_respuesta varchar (999),
@@ -16,9 +18,20 @@
 --fecha date,
 --Id_categoria_T integer);
 
---Create Table Categorias
---(Id_Categoria integer not null primary key,
---Id_categoria_T integer,
---Id_categoria_p integer,
---Descripcion varchar (999),
---Nombre Varchar (999))
+Create Table Categorias
+(Id_Categoria integer not null,
+Id_categoria_T integer,
+Id_categoria_p integer,
+Descripcion varchar (999),
+Nombre Varchar (999),
+primary key(Id_Categoria,Id_categoria_T,Id_categoria_p)
+)
+
+--alter table Respuestas
+--add constraint Preguntas Foreign key (id_pregunta)
+--references Preguntas (Id_pregunta)
+
+--alter table Preguntas
+--add constraint Test Foreign key (id_Test)
+--references Test (Id_Test)
+
